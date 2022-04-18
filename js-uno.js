9f2193d2e557e470eab036d4e80a461048dbeb6d
judith carreño <3280kg@gmail.com>
2022-04-18 00:45:41 -0400

    primer commit

---
+++ js-uno.js
@@ -0,0 +1,13 @@
+
+function queComer(horario){
+    if(horario === 'ocho'){
+        document.write("desayuno")
+    }
+    else if(horario === 'una'){
+        document.write("almuerzo")
+    }
+    else if(horario === 'siete'){
+        document.write("tomar 11")
+    }
+}
+queComer('ocho')
